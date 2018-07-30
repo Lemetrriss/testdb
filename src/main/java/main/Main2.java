afb3c4635659595c3ca84da983250d6e3b4652c1
lemetrriss <lemetrriss@gmail.com>
2018-07-30 18:41:13 +0300

    Home work DB + SQL

---
+++ src/main/java/main/Main2.java
@@ -0,0 +1,49 @@
+package main;
+
+import java.sql.*;
+import java.util.Properties;
+
+public class Main2 {
+    public static void main(String[] args) {
+        new Main2().run();
+    }
+
+    private void run() {
+        Properties properties = new Properties();
+        properties.setProperty("user","Lemetriss");
+        properties.setProperty("password","123");
+
+        try {
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/test", properties);
+            createTable(con);
+            Student student = new Student("Dmitrij", "Dmitrenko", 35);
+            fillTable(con, student);
+            dropTable(con);
+            con.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void dropTable(Connection con) throws SQLException {
+        Statement statement = con.createStatement();
+        statement.executeUpdate("DROP TABLE IF EXISTS student");
+        statement.close();
+    }
+
+    private void fillTable(Connection con, Student student) throws SQLException {
+        PreparedStatement preparedStatement = con.prepareStatement("INSERT INTO test.student (name, lastName, age) VALUES (?,?,?)");
+        preparedStatement.setString(1, student.getName());
+        preparedStatement.setString(2, student.getLastName());
+        preparedStatement.setInt(3, student.getAge());
+        preparedStatement.executeUpdate();
+        preparedStatement.close();
+    }
+
+    private void createTable(Connection con) throws SQLException {
+        Statement statement = con.createStatement();
+        statement.executeUpdate(
+                "CREATE TABLE student (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, name VARCHAR(30), lastName VARCHAR(50), age INT)");
+        statement.close();
+    }
+}
---
+++ src/main/java/main/Student.java
@@ -0,0 +1,66 @@
+package main;
+
+public class Student {
+    private int id;
+    private String name;
+    private String lastName;
+    private int age;
+
+    public Student(int id, String name, String lastName, int age) {
+        this.id = id;
+        this.name = name;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    public Student(String name, String lastName, int age) {
+        this.name = name;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    public Student() {
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
